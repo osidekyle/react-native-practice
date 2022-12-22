6c474bec2dcabf175cd3e031bc05eb3b2b870f55
osidekyle <hawkinskyle67@gmail.com>
2022-12-22 00:10:20 -0800

    simple additions

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   console.log("App executed")
+
+  const handPress = () => {
+    console.log("text pressed")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={onPress}>Hello. World!</Text>
+      <Image source={{width: 200, height: 300, uri: "https://picsum.photos/200/300"}} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
 });
